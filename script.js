d2e2c7672095140bbe9201d509a92733abc30384
ParasTemkar <parastemkar0204@gmail.com>
2025-02-12 22:43:46 +0530

    Add logic to store number  buttoninput.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+const numbers = document.querySelectorAll('.number');
+
+numbers.forEach(number => {
+
+    if (num1 === undefined) {
+    number.addEventListener('click', (e) => {
+        const num1 = e.target.id;
+        return num1;
+    })}
+    else {
+        number.addEventListener('click', (e) => {
+            const num2 = e.target.id;
+            return num2;
+    })}
+    });
+
+
