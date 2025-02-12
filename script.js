aa1ab66a525b1436fdcf60dffe1ad70669b83fa7
ParasTemkar <parastemkar0204@gmail.com>
2025-02-12 23:00:27 +0530

    Add logic for performing operations.

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-
 const numbers = document.querySelectorAll('.number');
 
+let num1 = '';
+let num2 = '';
+let operator = '';
+
 numbers.forEach(number => {
-
-    if (num1 === undefined) {
     number.addEventListener('click', (e) => {
-        const num1 = e.target.id;
-        return num1;
-    })}
-    else {
-        number.addEventListener('click', (e) => {
-            const num2 = e.target.id;
-            return num2;
-    })}
+        if (operator === '') {
+            num1 += e.target.id;
+            document.getElementById('result').innerHTML = num1;
+        } else {
+            num2 += e.target.id;
+            document.getElementById('result').innerHTML = num2;
+        }
     });
+});
 
+// All the operators
+const addition = document.getElementById('add');
+addition.addEventListener('click', () => {
+    operator = '+';
+});
+
+const subtraction = document.getElementById('sub');
+subtraction.addEventListener('click', () => {
+    operator = '-';
+});
+
+const multiplication = document.getElementById('multi');
+multiplication.addEventListener('click', () => {
+    operator = '*';
+});
+
+const division = document.getElementById('div');
+division.addEventListener('click', () => {
+    operator = '/';
+});
 
